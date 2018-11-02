03992d4941e18ae290cc6f2b29c4d781624a6cf7
yangri <827969491@qq.com>
2018-11-02 19:30:06 +0800

    脚本

---
+++ db/jtm/DML/201811021924_jtm_ddl.sql
@@ -0,0 +1,12 @@
+﻿DROP TABLE IF EXISTS `jtbmatch`;
+
+CREATE TABLE `jtbmatch` ( 
+	`MatchId` int(11) NOT NULL AUTO_INCREMENT,
+	`MatchName` varchar(100) DEFAULT NULL,
+	`NatchDate` datetime DEFAULT NULL,
+	`RedTeam` varchar(50) DEFAULT NULL,
+	`BuleTeam` varchar(50) DEFAULT NULL,
+	`MatchResult` varchar(2)	DEFAULT NULL,
+	`MatchVideo` varchar(100) DEFAULT NULL,
+	PRIMARY KEY (`MatchId`)
+)	ENGINE=InnoDB DEFAULT CHARSET=utf8;
