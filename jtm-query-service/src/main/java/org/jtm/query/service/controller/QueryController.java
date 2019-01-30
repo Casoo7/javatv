1ebd3f11eed895f5ad12835ac664c0ea017d1cbe
yangri <827969491@qq.com>
2019-01-30 17:38:34 +0800

    javatv:增加公共服务组件项目

--- jtm-query-service/src/main/java/org/jtm/query/service/controller/QueryController.java
+++ jtm-query-service/src/main/java/org/jtm/query/service/controller/QueryController.java
@@ -1,16 +1,14 @@
-//package org.jtm.query.service.controller;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class QueryController {
-//	@Value("${jtm.server.port}")
-//	String foo;
-//	@RequestMapping("/query")
-//	public String hi(){
-//		
-//		return foo;
-//	}
-//}
+package org.jtm.query.service.controller;
+
+import org.jtm.common.util.CalUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QueryController {
+	@RequestMapping("/query")
+	public String hi(){
+		CalUtils cal = new CalUtils();
+		return cal.MergeDateOrderByDesc();
+	}
+}
