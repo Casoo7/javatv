b9a8c189111567b394c2e945e897c45191a39ed5
yangri <827969491@qq.com>
2019-04-16 19:43:49 +0800

    新增mybatis 文件

--- jtm-query-service/src/main/java/com/jtm/controller/QueryController.java
+++ jtm-query-service/src/main/java/com/jtm/controller/QueryController.java
@@ -3,12 +3,23 @@ package com.jtm.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jtm.dto.JBMatch;
+import com.jtm.mapper.JBMatchMapper;
 import com.jtm.util.CalUtils;
+import com.jtm.util.DateUtil;
 
 @RestController
 public class QueryController {
+	private JBMatchMapper jbMatchMapper;
+	
 	@RequestMapping("/query")
 	public String hi(){
+		JBMatch jbMatch = new JBMatch();
+		jbMatch.setBuleteam("team1");
+		jbMatch.setRedteam("team2");
+		jbMatch.setMaketime(DateUtil.getCurrentDate());
+		jbMatch.setModifytime(DateUtil.getCurrentDate());
+		jbMatchMapper.insert(jbMatch);
 		CalUtils cal = new CalUtils();
 		return cal.MergeDateOrderByDesc();
 	}
