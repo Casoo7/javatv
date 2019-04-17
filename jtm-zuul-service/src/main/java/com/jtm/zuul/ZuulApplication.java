9d63fa9fa684555221bb0c79c9a147fa79e164d3
casoo7 <827969491@qq.com>
2019-04-17 23:03:00 +0800

    增加配置文件

--- jtm-zuul-service/src/main/java/com/jtm/zuul/ZuulApplication.java
+++ jtm-zuul-service/src/main/java/com/jtm/zuul/ZuulApplication.java
@@ -2,12 +2,26 @@ package com.jtm.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @RequestMapping("/query")
+    public String hi(){
+        return "123456";
     }
 
 }
