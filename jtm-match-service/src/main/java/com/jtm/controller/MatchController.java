542b3762bff83aecb53236db927680553ff49cb3
yangri <827969491@qq.com>
2019-06-06 16:39:09 +0800

    返回数据格式由xml改为json

--- jtm-match-service/src/main/java/com/jtm/controller/MatchController.java
+++ jtm-match-service/src/main/java/com/jtm/controller/MatchController.java
@@ -1,5 +1,6 @@
 package com.jtm.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,14 +8,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping(value = "/match", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class MatchController {
 
     @RequestMapping("/match")
-    public Map test(){
-        Map map = new HashMap();
+    public Map<String,String> test(){
+        Map map = new HashMap<String,String>();
         map.put("name","we");
-        map.put("name","omg");
-        map.put("name","ig");
+        map.put("mobile","123456789");
+        map.put("email","iadfdasfasdg@qq.com");
         return map;
     }
 }
