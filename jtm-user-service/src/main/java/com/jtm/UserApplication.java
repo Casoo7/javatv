a8fe98677c701f3c56f947a56126f4a143072283
casoo7 <827969491@qq.com>
2019-11-27 21:36:27 +0800

    删除重复配置文件

--- jtm-user-service/src/main/java/com/jtm/UserApplication.java
+++ jtm-user-service/src/main/java/com/jtm/UserApplication.java
@@ -1,9 +1,17 @@
 package com.jtm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.jtm.mapper")
 public class UserApplication {
 
     public static void main(String[] args) {
