cfd96222a3d95a55888300842bf207908fbee98a
casoo7 <827969491@qq.com>
2019-11-27 21:09:29 +0800

    新增用户服务

---
+++ jtm-user-service/src/main/java/com/jtm/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.jtm.controller;
+
+import com.jtm.dto.JBMatch;
+import com.jtm.mapper.JBMatchDao;
+import com.jtm.mapper.JBMatchMapper;
+import com.jtm.util.DateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	private static Logger log = LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	private JBMatchDao jBMatchDao;
+	@Autowired
+	private JBMatchMapper jbMatchMapper;
+
+	// 压力测试游标
+	int a = 0;
+
+	@RequestMapping("/query")
+	public String hi(){
+		a++;
+		System.out.println("测试！！！！！第"+a+"次！");
+		log.debug("debug");
+		log.info("info");
+		log.error("error");
+		log.warn("warn");
+		log.trace("trace");
+		JBMatch jbMatch = new JBMatch();
+		jbMatch.setBuleteam("JDG");
+		jbMatch.setRedteam("IG");
+		jbMatch.setMaketime(DateUtil.getCurrentDate());
+		jbMatch.setModifytime(DateUtil.getCurrentDate());
+		jbMatchMapper.insert(jbMatch);
+		return jBMatchDao.findNameById("1");
+	}
+}
